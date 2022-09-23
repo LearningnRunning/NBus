5ad32a3c37f6d21107c1919c8e8eba8c138a91c8
Zero <wndud1375@gmail.com>
2022-09-23 13:33:18 +0900

    reservation edit

---
+++ reservation/service.py
@@ -0,0 +1,21 @@
+from reservation.vo import Reserve
+from reservation.dao import ReserveDao
+
+
+class ReService:
+    def __init__(self):
+        self.dao = ReserveDao()
+
+    # 추가
+    def addReserve(self, r:Reserve):
+        self.dao.insert(r)
+
+    def getById(self, id:str)->Reserve: #타입 힌트
+        return self.dao.select(id)
+
+    # 삭제
+    def delReserve(self, id:str):
+        self.dao.delete(id)
+
+    def editReserve(self, r:Reserve):
+        self.dao.update(r)
--- reservation/vo.py
+++ reservation/vo.py
@@ -1,17 +1,18 @@
 class Reserve:
-    def __init__(self, reservenum: int = 0, id: str = None, arrmsg: str = None, plainNo: str = None,
-                stNm: str = None, stNmD: str = None, reserve: str = None, etc: str = None):
+    def __init__(self, reservenum: int = 0, id: str = None, arrmsg: str = None, rtNm: str = None,
+                 plainNo: str = None, stNm: str = None, stNmD: str = None, reserve: str = None, etc: str = None):
         self.reservenum = reservenum
         self.id = id
-        self.arrmsg = arrmsg
-        self.plainNo = plainNo
-        self.stNm = stNm
-        self.stNmD = stNmD
-        self.reserve = reserve
-        self.etc = etc
+        self.arrmsg = arrmsg  # 도착예정시간
+        self.rtNm = rtNm  # 도착예정시간
+        self.plainNo = plainNo # 차량 번호
+        self.stNm = stNm # 승차 정류소명
+        self.stNmD = stNmD # 하차정류소명
+        self.reserve = reserve # 예약 확인
+        self.etc = etc  # 예약 취소 사유
 
 
     def __str__(self): #toString()
-        return 'id :'+self.id+' / '+'arrmsg : '+self.arrmsg+' / '+ ' plainNo :' + self.plainNo + ' / '\
-               + 'stNm :' + self.stNm+ ' / ' + 'stNmD :' + self.stNmD+ ' / ' 'reserve :' + self.reserve+ ' / ' \
-               +'etc :' + self.etc
+        return 'id :'+self.id+' / '+'arrmsg : '+self.arrmsg+' / '+ ' plainNo :' +self.plainNo+ ' / '\
+               +' rtNm :' +self.rtNm+ ' / '+ 'stNm :' +self.stNm+ ' / ' + 'stNmD :' +self.stNmD+ ' / '\
+               +'reserve :' +self.reserve+ ' / ' +'etc :' +self.etc
